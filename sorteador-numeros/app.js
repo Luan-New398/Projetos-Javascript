1b66744e5a80782ab71556b4d755793999e05c01
Luan-New398 <luannewtybucheski@gmail.com>
2024-12-19 14:16:07 -0300

    Aula 7, agr funciona +-

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,8 +1,27 @@
 function sortear() {
 
-        let Q = document.getElementById("quantidade").value;
-        let Min = document.getElementById("de").value;
-        let Max = document.getElementById("ate").value;
-        alert(`Quantidade: ${Q} Minimo: ${Min} Máximo: ${Max}`)
+        let Q = parseInt(document.getElementById("quantidade").value);
+        let Min = parseInt(document.getElementById("de").value);
+        let Max = parseInt(document.getElementById("ate").value);
+
+        /*alert(`Quantidade: ${Q} Minimo: ${Min} Máximo: ${Max}`)
+        let N = RNG(Min, Max);
+        alert(N);*/
+
+        let Ns = [];
+        for (let i = 0; i < Q; i++) {
+
+            Ns.push(RNG(Min,Max));
+            //Forma diferente que a da Alura
+
+        }
+
+        alert(Ns);
+
+}
+
+function RNG(min, max) {
+    
+    return(Math.floor(Math.random() * (max - min + 1)) + min);
 
 }
