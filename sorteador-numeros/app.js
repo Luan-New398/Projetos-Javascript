5537382f96a29d0d549ab2863f0c016e39992067
Luan-New398 <luannewtybucheski@gmail.com>
2024-12-19 13:57:09 -0300

    Primeira mudança app.js

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,8 @@
+function sortear() {
+
+        let Q = document.getElementById("quantidade").value;
+        let Min = document.getElementById("de").value;
+        let Max = document.getElementById("ate").value;
+        alert(`Quantidade: ${Q} Minimo: ${Min} Máximo: ${Max}`)
+
+}
